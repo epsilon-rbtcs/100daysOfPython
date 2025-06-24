284e0558b5e90683832758019aa2a7377c2fd7a0
epsilon-rbtcs <abhadra1606@gmail.com>
2025-06-25 00:53:14 +0530

    Day 1 Project Update

---
+++ Band Name Generator.py
@@ -0,0 +1,6 @@
+# Day 1 of 100 Days Of Python - Band Name Generator
+
+print("Welcome to Band Name Generator Program\nWe can generate amazing band name for you !!")
+a = input("Enter the city you grew up in :\n")
+b = input("Enter the name of your pet :\n")
+print("Your band name could be",a + " " + b)
